773749bc5391151ab564bcf29250a2da9ff8d0ec
Mikey Henderson <4452096+fiddlermikey@users.noreply.github.com>
2024-03-18 12:20:55 -0700

    Release 1.0 (#5)
    * chore(version1): Rewrite with dependency injection pattern and unit tests
    * chore(docs): Refactor doc structure and add detail for clarity
    * fix(manifest): Update manifest class names for jobs
    * chore(manifest): Add UO framework version
    * chore(docs): Fix jargon in Overview
    * fix(ci): Rename Keyfactor workflow to keyfactor-bootstrap-workflow.yml

---
+++ AzureEnterpriseApplicationOrchestrator.Tests/Client.cs
@@ -0,0 +1,216 @@
+// Copyright 2024 Keyfactor
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.Extensions.Logging;
+using Keyfactor.Logging;
+using NLog.Extensions.Logging;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using AzureEnterpriseApplicationOrchestrator.Client;
+
+namespace AzureEnterpriseApplicationOrchestrator.Tests;
+
+public class AzureEnterpriseApplicationOrchestrator_Client
+{
+    public AzureEnterpriseApplicationOrchestrator_Client()
+    {
+        ConfigureLogging();
+    }
+
+    [IntegrationTestingFact]
+    public void GraphClient_Application_AddGetRemove_ReturnSuccess()
+    {
+        // Arrange
+        const string password = "passwordpasswordpassword";
+        string certName = "AppTest" + Guid.NewGuid().ToString()[..6];
+        X509Certificate2 ssCert = GetSelfSignedCert(certName);
+        string b64PfxSslCert = Convert.ToBase64String(ssCert.Export(X509ContentType.Pfx, password));
+
+        IntegrationTestingFact env = new();
+
+        IAzureGraphClient client = new GraphClient.Builder()
+            .WithTenantId(env.TenantId)
+            .WithApplicationId(env.ApplicationId)
+            .WithClientSecret(env.ClientSecret)
+            .WithTargetApplicationId(env.TargetApplicationId)
+            .Build();
+
+        // Step 1 - Add the certificate to the Application
+
+        client.AddApplicationCertificate(certName, b64PfxSslCert, password);
+
+        // Assert
+        // The certificate should be added to the Application.
+        // If this is not the case, AddApplicationCertificate will throw an exception.
+
+        // Step 2 - Get the certificate from the Application
+
+        // Act
+        OperationResult<IEnumerable<Keyfactor.Orchestrators.Extensions.CurrentInventoryItem>> operationResult = client.GetApplicationCertificates();
+        
+        // Assert
+        Assert.True(operationResult.Success);
+        Assert.NotNull(operationResult.Result);
+        Assert.True(operationResult.Result.Any(c => c.Alias == certName));
+        
+        // Step 3 - Determine if the certificate exists in the Application
+
+        // Act
+        bool exists = client.ApplicationCertificateExists(certName);
+
+        // Assert
+        Assert.True(exists);
+
+        // Step 4 - Remove the certificate from the Application
+        client.RemoveApplicationCertificate(certName);
+
+
+        // Assert
+        // The certificate should be removed from the Application.
+        // If this is not the case, RemoveApplicationCertificate will throw an exception.
+
+        // Step 5 - Determine if the certificate exists in the Application
+
+        // Act
+        exists = client.ApplicationCertificateExists(certName);
+
+        // Assert
+        Assert.False(exists);
+    }
+
+    [IntegrationTestingFact]
+    public void GraphClient_ServicePrincipal_AddGetRemove_ReturnSuccess()
+    {
+        // Arrange
+        const string password = "passwordpasswordpassword";
+        string certName = "SPTest" + Guid.NewGuid().ToString()[..6];
+        X509Certificate2 ssCert = GetSelfSignedCert(certName);
+        string b64PfxSslCert = Convert.ToBase64String(ssCert.Export(X509ContentType.Pfx, password));
+
+        IntegrationTestingFact env = new();
+
+        IAzureGraphClient client = new GraphClient.Builder()
+            .WithTenantId(env.TenantId)
+            .WithApplicationId(env.ApplicationId)
+            .WithClientSecret(env.ClientSecret)
+            .WithTargetApplicationId(env.TargetApplicationId)
+            .Build();
+
+        // Step 1 - Add the certificate to the Service Principal (and set it as the preferred SAML signing certificate)
+
+        client.AddServicePrincipalCertificate(certName, b64PfxSslCert, password);
+
+        // Assert
+        // The certificate should be added to the Service Principal and set as the preferred SAML signing certificate.
+        // If this is not the case, AddServicePrincipalCertificate will throw an exception.
+
+        // Step 2 - Get the certificate from the Service Principal
+        OperationResult<IEnumerable<Keyfactor.Orchestrators.Extensions.CurrentInventoryItem>> operationResult = client.GetServicePrincipalCertificates();
+
+        // Assert
+        Assert.True(operationResult.Success);
+        Assert.NotNull(operationResult.Result);
+        Assert.True(operationResult.Result.Any(c => c.Alias == certName));
+
+        // Step 3 - Determine if the certificate exists in the Service Principal
+
+        // Act
+        bool exists = client.ServicePrincipalCertificateExists(certName);
+
+        // Assert
+        Assert.True(exists);
+
+        // Step 4 - Remove the certificate from the Service Principal
+        client.RemoveServicePrincipalCertificate(certName);
+
+
+        // Assert
+        // The certificate should be removed from the Service Principal.
+        // If this is not the case, RemoveServicePrincipalCertificate will throw an exception.
+
+        // Step 5 - Determine if the certificate exists in the Service Principal
+
+        // Act
+        exists = client.ServicePrincipalCertificateExists(certName);
+
+        // Assert
+        Assert.False(exists);
+    }
+
+    [IntegrationTestingFact]
+    public void GraphClient_DiscoverApplicationIds_ReturnSuccess()
+    {
+        // Arrange
+        const string password = "passwordpasswordpassword";
+        string certName = "SPTest" + Guid.NewGuid().ToString()[..6];
+        X509Certificate2 ssCert = GetSelfSignedCert(certName);
+        string b64PfxSslCert = Convert.ToBase64String(ssCert.Export(X509ContentType.Pfx, password));
+
+        IntegrationTestingFact env = new();
+
+        IAzureGraphClient client = new GraphClient.Builder()
+            .WithTenantId(env.TenantId)
+            .WithApplicationId(env.ApplicationId)
+            .WithClientSecret(env.ClientSecret)
+            .WithTargetApplicationId(env.TargetApplicationId)
+            .Build();
+
+        // Act
+        OperationResult<IEnumerable<string>> operationResult = client.DiscoverApplicationIds();
+
+        // Assert
+        Assert.True(operationResult.Success);
+        Assert.NotNull(operationResult.Result);
+        Assert.True(operationResult.Result.Any());
+    }
+
+    public static X509Certificate2 GetSelfSignedCert(string hostname)
+    {
+        RSA rsa = RSA.Create(2048);
+        CertificateRequest req = new CertificateRequest($"CN={hostname}", rsa, HashAlgorithmName.SHA256,
+                RSASignaturePadding.Pkcs1);
+
+        SubjectAlternativeNameBuilder subjectAlternativeNameBuilder = new SubjectAlternativeNameBuilder();
+        subjectAlternativeNameBuilder.AddDnsName(hostname);
+        req.CertificateExtensions.Add(subjectAlternativeNameBuilder.Build());
+        req.CertificateExtensions.Add(new X509KeyUsageExtension(X509KeyUsageFlags.DataEncipherment | X509KeyUsageFlags.KeyEncipherment | X509KeyUsageFlags.DigitalSignature, false));        
+        req.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension(new OidCollection { new Oid("2.5.29.32.0"), new Oid("1.3.6.1.5.5.7.3.1") }, false));
+
+        X509Certificate2 selfSignedCert = req.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(5));
+        Console.Write($"Created self-signed certificate for \"{hostname}\" with thumbprint {selfSignedCert.Thumbprint}\n");
+        return selfSignedCert;
+    }
+
+    static void ConfigureLogging()
+    {
+        var config = new NLog.Config.LoggingConfiguration();
+
+        // Targets where to log to: File and Console
+        var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+        logconsole.Layout = @"${date:format=HH\:mm\:ss} ${logger} [${level}] - ${message}";
+
+        // Rules for mapping loggers to targets            
+        config.AddRule(NLog.LogLevel.Trace, NLog.LogLevel.Fatal, logconsole);
+
+        // Apply config           
+        NLog.LogManager.Configuration = config;
+
+        LogHandler.Factory = LoggerFactory.Create(builder =>
+                {
+                builder.AddNLog();
+                });
+    }
+
+}
+
---
+++ AzureEnterpriseApplicationOrchestrator.Tests/FakeClient.cs
@@ -0,0 +1,177 @@
+// Copyright 2024 Keyfactor
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using AzureEnterpriseApplicationOrchestrator.Client;
+using Keyfactor.Logging;
+using Keyfactor.Orchestrators.Common.Enums;
+using Keyfactor.Orchestrators.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace AzureEnterpriseApplicationOrchestrator.Tests;
+
+public class FakeClient : IAzureGraphClient
+{
+
+    public class FakeBuilder : IAzureGraphClientBuilder
+    {
+        private FakeClient _client = new FakeClient();
+
+        public string? _tenantId { get; set; }
+        public string? _targetApplicationId { get; set; }
+        public string? _applicationId { get; set; }
+        public string? _clientSecret { get; set; }
+        public string? _azureCloudEndpoint { get; set; }
+
+        public IAzureGraphClientBuilder WithTenantId(string tenantId)
+        {
+            _tenantId = tenantId;
+            return this;
+        }
+
+        public IAzureGraphClientBuilder WithTargetApplicationId(string applicationId)
+        {
+            _targetApplicationId = applicationId;
+            return this;
+        }
+
+        public IAzureGraphClientBuilder WithApplicationId(string applicationId)
+        {
+            _applicationId = applicationId;
+            return this;
+        }
+
+        public IAzureGraphClientBuilder WithClientSecret(string clientSecret)
+        {
+            _clientSecret = clientSecret;
+            return this;
+        }
+
+        public IAzureGraphClientBuilder WithAzureCloud(string azureCloud)
+        {
+            _azureCloudEndpoint = azureCloud;
+            return this;
+        }
+
+        public IAzureGraphClient Build()
+        {
+            return _client;
+        }
+    }
+
+
+    ILogger _logger = LogHandler.GetClassLogger<FakeClient>();
+
+    public IEnumerable<string>? ApplicationIdsAvailableOnFakeTenant { get; set; }
+    public Dictionary<string, string>? CertificatesAvailableOnFakeTarget { get; set; }
+
+    public void AddApplicationCertificate(string certificateName, string certificateData, string certificatePassword)
+    {
+        _logger.LogDebug($"Adding certificate {certificateName} to fake application");
+
+        if (CertificatesAvailableOnFakeTarget == null)
+        {
+            CertificatesAvailableOnFakeTarget = new Dictionary<string, string>();
+        }
+
+        _logger.LogDebug($"Adding certificate {certificateName} to fake application");
+
+        CertificatesAvailableOnFakeTarget.Add(certificateName, certificateData);
+
+        _logger.LogTrace($"Fake client has {CertificatesAvailableOnFakeTarget.Count} certificates in inventory");
+    }
+
+    public void AddServicePrincipalCertificate(string certificateName, string certificateData, string certificatePassword)
+    {
+        AddApplicationCertificate(certificateName, certificateData, certificatePassword);
+    }
+
+    public OperationResult<IEnumerable<string>> DiscoverApplicationIds()
+    {
+        if (ApplicationIdsAvailableOnFakeTenant == null)
+        {
+            throw new Exception("Discover Application IDs method failure - no application ids set");
+        }
+
+        return new OperationResult<IEnumerable<string>>(ApplicationIdsAvailableOnFakeTenant);
+    }
+
+    public OperationResult<IEnumerable<CurrentInventoryItem>> GetApplicationCertificates()
+    {
+        _logger.LogDebug("Getting Application Certificates from fake Application");
+
+        if (CertificatesAvailableOnFakeTarget == null)
+        {
+            throw new Exception("Get Application Certificate method failure - no inventory items set");
+        }
+
+        List<CurrentInventoryItem> inventoryItems = new List<CurrentInventoryItem>();
+        OperationResult<IEnumerable<CurrentInventoryItem>> result = new(inventoryItems);
+
+        foreach (KeyValuePair<string, string> cert in CertificatesAvailableOnFakeTarget)
+        {
+            inventoryItems.Add(new CurrentInventoryItem
+            {
+                Alias = cert.Key,
+                PrivateKeyEntry = false,
+                ItemStatus = OrchestratorInventoryItemStatus.Unknown,
+                UseChainLevel = true,
+                Certificates = new List<string> { cert.Value }
+            });
+        }
+
+        _logger.LogDebug($"Fake client has {inventoryItems.Count} certificates in inventory");
+
+        return result;
+    }
+
+    public OperationResult<IEnumerable<CurrentInventoryItem>> GetServicePrincipalCertificates()
+    {
+        return GetApplicationCertificates();
+    }
+
+    public void RemoveApplicationCertificate(string certificateName)
+    {
+        if (CertificatesAvailableOnFakeTarget == null || !CertificatesAvailableOnFakeTarget.ContainsKey(certificateName))
+        {
+            throw new Exception("Certificate not found");
+        }
+
+        _logger.LogDebug($"Removing certificate {certificateName} from fake Application");
+
+        CertificatesAvailableOnFakeTarget.Remove(certificateName);
+
+        _logger.LogTrace($"Fake client has {CertificatesAvailableOnFakeTarget.Count} certificates in inventory");
+        return;
+    }
+
+    public void RemoveServicePrincipalCertificate(string certificateName)
+    {
+        RemoveApplicationCertificate(certificateName);
+    }
+
+    public bool ApplicationCertificateExists(string certificateName)
+    {
+        if (CertificatesAvailableOnFakeTarget != null)
+        {
+            return CertificatesAvailableOnFakeTarget.ContainsKey(certificateName);
+        }
+
+        return false;
+    }
+
+    public bool ServicePrincipalCertificateExists(string certificateName)
+    {
+        return ApplicationCertificateExists(certificateName);
+    }
+}
